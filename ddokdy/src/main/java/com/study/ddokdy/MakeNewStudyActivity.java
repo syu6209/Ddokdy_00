808685e6156d9a74772d305193b232d18451ff11
syu6209 <syu6209@naver.com>
2016-05-24 14:36:57 +0900

    0524

---
+++ ddokdy/src/main/java/com/study/ddokdy/MakeNewStudyActivity.java
@@ -0,0 +1,13 @@
+package com.study.ddokdy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MakeNewStudyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_new_study);
+    }
+}
